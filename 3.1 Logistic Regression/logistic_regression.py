03f66e53d36aadf921ec23abb8210a92c7a01533
Shayan Sadar <shayansadar37@gmail.com>
2019-01-26 14:59:08 -0600

    Final Logistic Regression Commit

--- 3.1 Logistic Regression/logistic_regression.py
+++ 3.1 Logistic Regression/logistic_regression.py
@@ -33,6 +33,7 @@ from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(Y_test, Y_pred)
 print(cm) #shows the number of correct predictions made
 
+'''
 #Visualizing Training Set Results
 from matplotlib.colors import ListedColormap
 X_set, Y_set = X_train, Y_train
@@ -49,5 +50,25 @@ plt.title('Logistic Regression (Training Set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
+plt.show()'''
+
+#Visualizing Testing Set Results
+from matplotlib.colors import ListedColormap
+X_set, Y_set = X_test, Y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                    np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                                       alpha = 0.75, cmap = ListedColormap(('red','green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate (np.unique(Y_set)):
+    plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Classify users if they buy the Car')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
 plt.show()
+
+
 
